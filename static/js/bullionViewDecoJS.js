6f45fb56d7761d1059d006a847e3b0d0bf75281d
vivek <vivek.303826@gmail.com>
2021-07-21 16:22:40 +0530

    updated bullionView page and its corresponding js page bullionViewDecoJs

--- static/js/bullionViewDecoJS.js
+++ static/js/bullionViewDecoJS.js
@@ -2,6 +2,7 @@ window.onload = function(){
     console.log('deco bullion view connect');
     runFunctions();
     listDecoration();
+    showBullionTransaction('all');
 }
 
 function runFunctions(){
@@ -67,5 +68,69 @@ function listDecoration(){
             element.classList.add('bg-light');
         }else{element.classList.add('bg-white')}
         count +=1;
+    });
+}
+
+function showBullionTransaction(bullionType){ //console.log(bullionType);
+    if(bullionType==='all'){
+        document.querySelector('.allBtn').classList.add('bg-primary','text-white')
+        document.querySelectorAll('.allList').forEach(element=>{
+            element.classList.remove('d-none');
+        });
+        inactiveBullionTransaction(bullionType);
+    }else if(bullionType==='9999_bullion'){
+        document.querySelector('.fineBtn').classList.add('bg-primary','text-white');
+        document.querySelectorAll('.fineBullionList').forEach(element=>{
+            element.classList.remove('d-none');
+        });
+        inactiveBullionTransaction(bullionType);
+
+    }else if(bullionType==='kachi'){
+        document.querySelector('.kachiBtn').classList.add('bg-primary','text-white');
+        document.querySelectorAll('.kachiBullionList').forEach(element=>{
+            element.classList.remove('d-none');
+        });
+        inactiveBullionTransaction(bullionType);
+
+    }else if(bullionType==='choursa'){
+        document.querySelector('.choursaBtn').classList.add('bg-primary','text-white');
+        document.querySelectorAll('.choursaBullionList').forEach(element=>{
+            element.classList.remove('d-none');
+        });
+        inactiveBullionTransaction(bullionType);
+    }else{
+
+    }
+}
+
+function inactiveBullionTransaction(bullionType){ // the role of this func is to inactivate all the other list 
+    var allListClass = ['.fineBullionList','.kachiBullionList','.choursaBullionList'];
+    var allBtn = ['.fineBtn','.kachiBtn','.choursaBtn'];
+    var fineListClass=['.allList','.kachiBullionList','.choursaBullionList']; //9999 bullion
+    var fineBtn = ['.allBtn','.kachiBtn','.choursaBtn'];//9999 bullion
+    var kachiListClass =['.allList','.fineBullionList','.choursaBullionList'];
+    var kachiBtn = ['.allBtn','.fineBtn','.choursaBtn'];
+    var choursaListClass =['.allList','.fineBullionList','.kachiBullionList'];
+    var choursaBtn = ['.allBtn','.fineBtn','.kachiBtn'];
+    if(bullionType==='all'){
+        inactiveStateFunc(allListClass,allBtn);
+    }
+    if(bullionType==='9999_bullion'){
+        inactiveStateFunc(fineListClass,fineBtn);
+    }
+    if(bullionType==='kachi'){
+        inactiveStateFunc(kachiListClass,kachiBtn); 
+    }
+    if(bullionType==='choursa'){
+        inactiveStateFunc(choursaListClass,choursaBtn);
+    }
+}
+
+function inactiveStateFunc(allListClass,allBtn){
+    allListClass.map(element=>{
+        document.querySelectorAll(element).forEach(element=>{element.classList.add('d-none')});
+    });
+    allBtn.map(element=>{
+        document.querySelectorAll(element).forEach(element=>{element.classList.remove('bg-primary','text-white')});
     });
 }
